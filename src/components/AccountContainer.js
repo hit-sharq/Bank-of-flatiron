4a441a2a8ddb227c4d272c92778da6c46d16f64a
Seth Mwebi <sethmwebi27@gmail.com>
2024-08-06 11:02:55 +0300

    added fetch functionality using useeffect in Accounts

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,6 +4,24 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      try {
+        const response = await fetch("http://localhost:8001/transactions");
+        if (!response.ok) {
+          throw new Error("Failed to fetch transactions");
+        }
+        const data = await response.json();
+        setTransactions(data);
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+
+    fetchTransactions();
+  }, []);
   return (
     <div>
       <Search />
